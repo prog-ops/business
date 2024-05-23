05b04540096e388c772b552fc5163690f5928856
prog-ops <akunbaru14102019@gmail.com>
2024-05-23 13:58:27 +0700

    Custom hooks

---
+++ frontend/src/app/hooks/useNodeAPI.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const YelpContext = createContext();
+
+const backendUrl = 'http://localhost:3001/api/yelp'
+
+export const YelpProvider = ({ children }) => {
+    const [businesses, setBusinesses] = useState([]);
+    const [offset, setOffset] = useState(0);
+    const limit = 5;
+
+    const loadBusinesses = async (reset = false) => {
+        try {
+            const response = await fetch(`${backendUrl}?offset=${reset ? 0 : offset}&limit=${limit}`);
+            const data = await response.json();
+            setBusinesses(reset ? data.businesses : [...businesses, ...data.businesses]);
+            if (!reset) setOffset(offset + limit);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <YelpContext.Provider value={{ businesses, loadBusinesses }}>
+            {children}
+        </YelpContext.Provider>
+    );
+};
+
+export const useNodeAPI = () => {
+    const context = useContext(YelpContext);
+    if (context === undefined) {
+        throw new Error('useYelp must be used within a YelpProvider');
+    }
+    return context;
+};
